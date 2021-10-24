845d48225d980525210721f0ca9884d4af5ce9b8
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-10-24 16:25:37 -0300

    added lowercases

---
+++ ptbrcap.php
@@ -0,0 +1,20 @@
+<?php
+
+$lowercases = [
+    'a',
+    'com',
+    'da',
+    'das',
+    'de',
+    'do',
+    'dos',
+    'e',
+    'em',
+    'na',
+    'nas',
+    'no',
+    'nos',
+    'o',
+    'por',
+    'sem'
+];
