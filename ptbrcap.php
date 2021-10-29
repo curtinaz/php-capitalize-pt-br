aab5430b5c3056c13b475ef2d65c9795a4e8c616
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-10-29 16:33:15 -0300

    Update ptbrcap.php

--- ptbrcap.php
+++ ptbrcap.php
@@ -1,39 +1,40 @@
 <?php
 
-$lowercases = [
-    'a',
-    'com',
-    'da',
-    'das',
-    'de',
-    'do',
-    'dos',
-    'e',
-    'em',
-    'na',
-    'nas',
-    'no',
-    'nos',
-    'o',
-    'por',
-    'sem'
-];
-
-$uppercases = [
-    'cnpj',
-    'cpf',
-    'ltda',
-    'qp',
-    'tv',
-    'mei',
-    'me',
-    'ei',
-    'epp',
-    'eireli',
-    'sa'
-];
-
 function ptbrcap ($phrase) {
+
+    $lowercases = [
+        'a',
+        'com',
+        'da',
+        'das',
+        'de',
+        'do',
+        'dos',
+        'e',
+        'em',
+        'na',
+        'nas',
+        'no',
+        'nos',
+        'o',
+        'por',
+        'sem'
+    ];
+    
+    $uppercases = [
+        'cnpj',
+        'cpf',
+        'ltda',
+        'qp',
+        'tv',
+        'mei',
+        'me',
+        'ei',
+        'epp',
+        'eireli',
+        'sa'
+    ];
+
     $eachWord = explode(' ', $phrase);
     for ($i = 0; $i < count($eachWord); $i++) {
         for ($j = 0; $j <= count($lowercases); $j++) {
