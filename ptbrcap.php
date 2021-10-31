b7e6f2f484dc9b0529f89d77803b27e1e4666d10
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-10-31 16:44:02 -0300

    Update ptbrcap.php

--- ptbrcap.php
+++ ptbrcap.php
@@ -36,18 +36,24 @@ function ptbrcap ($phrase) {
     ];
 
     $eachWord = explode(' ', $phrase);
+    $foundMatch = [];
     for ($i = 0; $i < count($eachWord); $i++) {
         for ($j = 0; $j <= count($lowercases); $j++) { // verifica se a palavra deveria ser lowercase.
             if ($eachWord[$i] == $lowercases[$j]) {
                 $eachWord[$i] = strtolower($eachWord[$i]);
+                $foundMatch[$i] = 1;
+                continue;
             }
         }
-        for ($j = 0; $j <= count($uppercases); $j++) { // verifica se a palavra deveria ser uppercase.
-            if ($eachWord[$i] == $uppercases[$j]) {
-                $eachWord[$i] = strtoupper($eachWord[$i]);
+        if($foundMatch[$i] == 0) {
+            for ($j = 0; $j <= count($uppercases); $j++) { // verifica se a palavra deveria ser uppercase.
+                if ($eachWord[$i] == $uppercases[$j]) {
+                    $eachWord[$i] = strtoupper($eachWord[$i]);
+                    $foundMatch[$i] = 1;
+                    continue;
+                }
             }
-        }
-        
+        }  
     }
     return $phrase;
 }
